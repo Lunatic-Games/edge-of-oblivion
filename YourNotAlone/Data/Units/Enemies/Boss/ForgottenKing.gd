61a1e7269f18eff720ae1b2fa9e73e64d18eded9
Matthias Harden <matthias@lunacylabs.ca>
2023-03-02 13:29:26 -0800

    add damaged particle and animation. Also remove an old print statement

--- YourNotAlone/Data/Units/Enemies/Boss/ForgottenKing.gd
+++ YourNotAlone/Data/Units/Enemies/Boss/ForgottenKing.gd
@@ -1,7 +1,6 @@
 extends "res://Data/Units/Enemies/Boss/Boss.gd"
 
 func choose_moveset():
-	print("Movesets => ", move_sets.size())
 	if move_sets.size() > 1:
 		if currentTile.is_tile_n_tiles_away(GameManager.player.currentTile, 1):
 			chosen_move = move_sets[1]
