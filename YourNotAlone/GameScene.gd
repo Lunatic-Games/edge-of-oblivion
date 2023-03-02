417b46d32fcca97311921e5c5931365ca2bea3f9
Matthias Harden <matthias@lunacylabs.ca>
2023-03-01 16:40:57 -0800

    add a winscreen

--- YourNotAlone/GameScene.gd
+++ YourNotAlone/GameScene.gd
@@ -10,6 +10,9 @@ func gameOver():
 	get_tree().paused = true
 	$Canvas/GameOver.visible = true
 
+func game_won():
+	get_tree().paused = true
+	$Canvas/Victory.visible = true
 
 func _on_MenuButton_pressed():
 	get_tree().change_scene_to(load("res://MainMenu.tscn"))
@@ -23,3 +26,9 @@ func _on_Restart_pressed():
 	yield(get_tree(), "idle_frame")
 	$Canvas/GameOver.visible = false
 	get_tree().change_scene_to(load("res://GameScene.tscn"))
+
+
+func _on_VictoryMainMenu_pressed():
+	get_tree().change_scene_to(load("res://MainMenu.tscn"))
+	GameManager.stop_game()
+	get_tree().paused = false
