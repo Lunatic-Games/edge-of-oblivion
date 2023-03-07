e6a7856c5290e1ae235b4be2a3dd4c9851f7f291
Matthias Harden <matthias@lunacylabs.ca>
2023-03-06 16:57:02 -0800

    Add highlight effects

--- YourNotAlone/Card.gd
+++ YourNotAlone/Card.gd
@@ -6,6 +6,8 @@ var itemData
 
 onready var tween = $Tween
 onready var animator = $AnimationPlayer
+onready var star_emitter = $WaterfallStarParticle
+onready var background = $Background
 
 func setup(resource, currentTier, animate):
 	$Sprite.texture = resource.sprite
@@ -31,3 +33,16 @@ func _on_Background_gui_input(event):
 		var player = get_tree().get_nodes_in_group("player")[0]
 		player.gainItem(itemData)
 		emit_signal("selectionMade")
+
+func _on_Background_mouse_entered():
+	tween.interpolate_property(self, "rect_scale", rect_scale, Vector2(1.2, 1.2), 0.3)
+	tween.interpolate_property(background, "color", background.color, Color("fa262333"), 0.3)
+	tween.start()
+	star_emitter.emitting = true
+
+
+func _on_Background_mouse_exited():
+	tween.interpolate_property(self, "rect_scale", rect_scale, Vector2(1.0, 1.0), 0.3)
+	tween.interpolate_property(background, "color", background.color, Color("dc262333"), 0.3)
+	tween.start()
+	star_emitter.emitting = false
