92b5808af746a92d02f562f5b2e0641a5e03986a
Matthias Harden <matthias@lunacylabs.ca>
2023-04-27 15:13:15 -0700

    dragon cloak hit effect

--- Data/Particles/BasicParticles.gd
+++ Data/Particles/BasicParticles.gd
@@ -1,7 +1,9 @@
 extends GPUParticles2D
 
+var extra_time_for_particles_to_fade: int = 10
+
 func _ready() -> void:
 	z_index = 2
 	emitting = true
-	await get_tree().create_timer(lifetime).timeout
+	await get_tree().create_timer(lifetime + extra_time_for_particles_to_fade).timeout
 	queue_free()
