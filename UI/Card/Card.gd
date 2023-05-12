a94e02aa6e91f6713350afaf20bc32101dbc4aed
Matthias Harden <matthias@lunacylabs.ca>
2023-05-12 11:29:07 -0700

    update card script

--- UI/Card/Card.gd
+++ UI/Card/Card.gd
@@ -9,24 +9,24 @@ var item_data: ItemData
 @onready var ray_animator: AnimationPlayer = $RayAnimator
 @onready var ray_emitter: GPUParticles2D = $RayParticles
 @onready var background: Control = $Background
-@onready var  card_name: RichTextLabel = $CardTop/Name
-@onready var card_sprite: TextureRect = $CardTop/TextureRect
+@onready var card_name: RichTextLabel = $CardTop/Name
+@onready var card_texture: TextureRect = $CardTop/TextureRect
 @onready var damage_icon_text: RichTextLabel = $CardTop/BaseIcons/DamageIcon/RichTextLabel
 @onready var activation_icon_text: RichTextLabel = $CardTop/BaseIcons/ActivationIcon/RichTextLabel
 @onready var card_description: RichTextLabel = $CardBottom/CardDescription
 
 func setup(card_item_data: ItemData, current_tier: int, animate: bool = true):
-	card_sprite.texture = card_item_data.sprite
-	card_name.text = "[center]" + card_item_data.item_name
+	card_texture.texture = card_item_data.sprite
+	card_name.text = "[center]" + card_item_data.item_name + "[/center]"
 	damage_icon_text.text = str(card_item_data.item_damage)
 	activation_icon_text.text = str(card_item_data.max_turn_timer)
 	match current_tier:
 		1: 
-			card_description.text = "[center]" + card_item_data.tier1Text
+			card_description.text = "[center]" + card_item_data.tier1Text + "[/center]"
 		2: 
-			card_description.text = "[center]" + card_item_data.tier2Text
+			card_description.text = "[center]" + card_item_data.tier2Text + "[/center]"
 		3: 
-			card_description.text = "[center]" + card_item_data.tier3Text
+			card_description.text = "[center]" + card_item_data.tier3Text + "[/center]"
 		
 	item_data = card_item_data
 	if animate:
@@ -49,7 +49,7 @@ func _on_Background_gui_input(event: InputEvent) -> void:
 func _on_Background_mouse_entered() -> void:
 	var tween: Tween = get_tree().create_tween().set_parallel(true)
 	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.3)
-	z_index = 1
+	z_index = 1 # Ensure the current card, and vfx appear over other cards
 	ray_emitter.emitting = true
 	ray_animator.play("rays_in")
 
