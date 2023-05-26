2a75a8fae7b7f757bfffd05145151944d02d7d16
Matthias Harden <matthias@lunacylabs.ca>
2023-05-26 14:43:49 -0700

    update pr

--- UI/HoverPopups/TextPopup/TextPopup.gd
+++ UI/HoverPopups/TextPopup/TextPopup.gd
@@ -1,48 +1,53 @@
 extends Button
 
-@export var display_string: String = "Lorem ipsum dolor iset imun babab baba chug wug!"
+@export_placeholder("Lorem ipsum dolor iset imun babab baba chug wug!") var display_string: String
 
-var delay: float = 0.75
-var time_hovered: float = 0.0
-var hovered: bool = false
-var spawned_popup: bool = false
+var delay_in_seconds: float = 0.5
+var time_hovered_in_seconds: float = 0.0
+var is_hovered: bool = false
+var is_popup_spawned: bool = false
 
 @onready var translation_container: Node2D = $TranslationContainer
 @onready var panel: PanelContainer = $TranslationContainer/BackgroundPanel
 @onready var popup_text: RichTextLabel = $TranslationContainer/BackgroundPanel/PanelContainer/InfoText
+@onready var animator: AnimationPlayer = $AnimationPlayer
 
-func _ready():
+func _ready() -> void:
 	popup_text.text = display_string
 
 func _on_mouse_entered() -> void:
-	hovered = true
+	is_hovered = true
 
 func _on_mouse_exited() -> void:
 	remove_text_popup()
-	time_hovered = 0
-	hovered = false
+	time_hovered_in_seconds = 0
+	is_hovered = false
 
-func _process(delta):
-	if hovered && !spawned_popup:
-		time_hovered += delta
-		if time_hovered >= delay:
+func _process(delta:float) -> void:
+	if is_hovered && !is_popup_spawned:
+		time_hovered_in_seconds += delta
+		if time_hovered_in_seconds >= delay_in_seconds:
 			spawn_text_popup()
 
-func spawn_text_popup():
-	spawned_popup = true
+func spawn_text_popup() -> void:
+	is_popup_spawned = true
 	translation_container.global_position = get_global_mouse_position() + Vector2(-10, -10)
 	translation_container.global_position -= panel.size
 	
-	var left_border = get_viewport().get_camera_2d().global_position.x - get_viewport_rect().size.x/2 + panel.size.x
-	var top_border = get_viewport().get_camera_2d().global_position.y - get_viewport_rect().size.y/3 + panel.size.y
+	var camera = get_viewport().get_camera_2d()
+	var viewport_size = get_viewport_rect().size
+	var left_border = camera.global_position.x - viewport_size.x / 2 + panel.size.x
+	var top_border = camera.global_position.y - viewport_size.y / 3 + panel.size.y # We divide by 3 here since the camera is represented as a 2x3 rectangle
 	
 	if translation_container.global_position.x < left_border:
 		translation_container.global_position.x = left_border
 	if translation_container.global_position.y < top_border:
 		translation_container.global_position.y = top_border
 	
-	translation_container.visible = true
+	animator.play("fade_in")
+	#translation_container.show()
 
-func remove_text_popup():
-	spawned_popup = false
-	translation_container.visible = false
+func remove_text_popup() -> void:
+	is_popup_spawned = false
+	animator.play("fade_out")
+	#translation_container.hide()
