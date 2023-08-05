0c531ab0e4c9477c106be9fc8db8f7c3264c6579
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-08-05 10:44:39 -0700

    Added enemy data

--- Data/Entities/_Components/Health/HEALTH_DATA.gd
+++ Data/Entities/_Components/Health/HEALTH_DATA.gd
@@ -2,7 +2,7 @@ class_name HealthData
 extends Resource
 
 
-@export var max_health: int = 100
+@export var max_health: int = 20
 @export var can_be_damaged: bool = true
 @export var can_be_killed: bool = true
 @export var can_be_healed: bool = true
