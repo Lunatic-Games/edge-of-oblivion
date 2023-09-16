356467d179c2d0fdb4310b23791b318de0776335
Davis Carlson <bigdavisc@users.noreply.github.com>
2023-09-16 12:57:38 -0700

    PK-222 New Tierable Int Variable Node (#130)
    * PK-216 Steamworks deployment workflow (#104)
    
    * Inital Godot Build
    
    * First complete version of godot builder workflow
    
    * Update godot-build-debug.yml to use pluggable workflow
    
    * Fixed path
    
    * Updated version location
    
    * Fixed var
    
    * Fixed more vars
    
    * Check if export_presets.cfg exists first
    
    * Moved the tee around
    
    * Missing presets
    
    * Update godot-build-debug.yml
    
    * Added check for build data output
    
    * Check for output file
    
    * Moved T inside
    
    * More tweaks
    
    * DOES THE FILE EXIST
    
    * fucking cat
    
    * maybe now?
    
    * pls lord
    
    * wHyyyyyy
    
    * Now it should work!
    
    * Fixed bash conditional
    
    * Testing matrix builds
    
    * jobs -> needs
    
    * Fixed dynamic variable
    
    * Maybe this time?
    
    * Testing
    
    * Missing s
    
    * Tweaked cleanup criteria
    
    * Fixed double dot
    
    * JS weirdness contains -> includes
    
    * Removed testing code from local machine
    
    * Testing dual windows and linux
    
    * Tweaked name
    
    * Update and rename godot-build-debug.yml to build-debug.yml
    
    Also changed to local reference for future merge
    
    * Delete export_presets.cfg
    
    * Initial script, not working
    
    * Fixed mistakes and consolidated
    
    * Rename .github/workflows/godot_build.yml to .github/workflows/pluggable/godot_build.yml
    
    * Switched to pluggable cleanup workflow
    
    * Rename .github/workflows/pluggable/artifact_cleanup.yml to .github/workflows/plug_artifact_cleanup.yml
    
    * Rename .github/workflows/pluggable/godot_build.yml to .github/workflows/plug_godot_build.yml
    
    * Updated references
    
    * Added perms needed for downstream flow
    
    * Removing unneeded env
    
    * Added artifact_retention_days input variable
    
    * Changed to use the pluggable build action and started testing artifact downloads
    
    * Split downloading into two calls so we can find the artifacts correctly
    
    * Fixed typo
    
    * Fixed another typo
    
    * Changed testing command
    
    * Changed linux extension from zip to x86_64
    
    * All ready to test steam deploys
    
    * Disabled auto branching for steam
    
    * Added version 4.2.5 of GodotSteam's GDExtension
    
    * Added dry run mode
    
    * Updated executable name and made app id a variable
    
    * Added a seperate artifact name variable
    
    * Updated how env variables are calculated
    
    * Added new artifact_name param to build call
    
    * Moved dry_run check for whole job after testing
    
    * Updated default description
    
    * First pass of TierableIntVariable
    
    * Got the ShortSword working
    
    * Finishing off merge
    
    * Implemented feedback
    
    * Added Forge level handling
    
    * Small refactoring
    
    * Added trigger hook to item setup to handle new forge level
    
    * Updated behavior to prevent null issue on new item

---
+++ LogicTree/Variables/TierableIntVariable.gd
@@ -0,0 +1,77 @@
+# NOTE: There is an invarient for this Node to work that is
+#       there must exist an {ITEM}.tres file (based on ITEM_DATA) in 
+#       the same folder as the {ITEM}.tscn file. Otherwise, bad stuff
+#       will follow.
+@tool
+@icon("res://Assets/art/logic-tree/variables/n-tier.png")
+class_name LT_TierableIntVariable
+extends LT_IntVariable
+
+
+const FORGE_VALUE_EXPORT_PREFIX: String = "forge_"
+const TIER_VALUE_EXPORT_PREFIX: String = "_tier_"
+
+var _tier_values: Dictionary = {}
+var max_tier = 1
+var max_forge = 1
+
+func _ready():
+	super._ready()
+	var this_item: Item = owner as Item
+	assert(this_item != null, "Owner is not an item for node: '" + name + "'")
+	this_item.tier_increased.connect(_on_tier_increased.bind(this_item))
+	this_item.setup_completed.connect(_on_tier_increased.bind(this_item))
+
+
+func _get_property_list() -> Array[Dictionary]:
+	
+	# This is where that very important invariant comes into play
+	var item_data = load(owner.scene_file_path.replace("tscn", "tres")) as ItemData
+	assert(item_data != null, "Unable to find tres file for '" + owner.name + "'")
+	max_tier = item_data.max_tier
+	max_forge = item_data.max_forge_level
+	
+	var properties: Array[Dictionary] = []
+	for forge_level in max_forge:
+		properties.append({
+			"name": "Forge Level " + str(forge_level + 1),
+			"hint_string": FORGE_VALUE_EXPORT_PREFIX + str(forge_level + 1),
+			"type": TYPE_NIL,
+			"usage": PROPERTY_USAGE_GROUP
+		})
+		for tier in max_tier:
+			properties.append({
+				"name": FORGE_VALUE_EXPORT_PREFIX + str(forge_level + 1) + TIER_VALUE_EXPORT_PREFIX + str(tier + 1),
+				"type": TYPE_INT,
+				"usage": PROPERTY_USAGE_DEFAULT
+			})
+	
+	return properties
+
+
+func _get(property: StringName) -> Variant:
+	if property.begins_with(FORGE_VALUE_EXPORT_PREFIX) and property.contains(TIER_VALUE_EXPORT_PREFIX):
+		if _tier_values.has(property):
+			return _tier_values[property]
+		return 0
+	
+	return null
+
+
+func _set(property: StringName, n: Variant) -> bool:
+	if property.begins_with(FORGE_VALUE_EXPORT_PREFIX) and property.contains(TIER_VALUE_EXPORT_PREFIX):
+		_tier_values[property] = n
+		if (property == FORGE_VALUE_EXPORT_PREFIX + "1" + TIER_VALUE_EXPORT_PREFIX + "1"):
+			default_value = n
+		return true
+	
+	return false
+
+
+func _on_tier_increased(item: Item) -> void:
+	value = _tier_values[FORGE_VALUE_EXPORT_PREFIX + str(item.forge_level) + TIER_VALUE_EXPORT_PREFIX + str(item.current_tier)]
+	
+
+func get_value_for_tier(forge: int, tier: int) -> int:
+	return _tier_values[FORGE_VALUE_EXPORT_PREFIX + str(forge) + TIER_VALUE_EXPORT_PREFIX + str(tier)]
+	
